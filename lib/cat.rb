21c4b4678c0e85d84ea36976762fef666a343f4c
Nicholas Patel <np4795@yahoo.com>
2020-02-26 02:20:03 +0000

    Automatically backed up by Learn

---
+++ lib/cat.rb
@@ -0,0 +1,20 @@
+class Cat
+  attr_reader :name 
+  attr_accessor :owner, :mood
+  @@all = []
+  
+  def initialize(name, owner)
+    @mood = "nervous"
+    @name = name
+    @owner = owner
+    @@all << self 
+   
+    
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  
+end
